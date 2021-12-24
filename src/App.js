e64adc25f51119d14ff47082f1eecdfb029a34b3
abhijeet0317 <94177479+abhijeet0317@users.noreply.github.com>
2021-12-24 18:41:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import './style.css';
+
+const App = () => {
+  const [bmi, setBmi] = useState();
+  const [info, setInfo] = useState();
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
+  const handleBmi = () => {
+    let val = (
+      [Number(weight) / Number(height) / Number(height)] * 10000
+    ).toFixed(1);
+    setBmi(val);
+    if (val < 18.5) {
+      setInfo("Under Weight");
+    } else if (val > 18.5 && val <= 24.9) {
+      setInfo("Healthy");
+    } else if (val > 24.9 && val < 30) {
+      setInfo("Overweight");
+    } else {
+      setInfo("Obese");
+    }
+  };
+  return (
+    <div className="block ui container">
+    <div className="ui form">
+      <h1 style={{textAlign: "center"}}>BMI Calculator</h1>
+      <label>Height : </label>
+      <input
+        type="text"
+        onChange={(e) => setHeight(e.target.value)}
+        placeholder="height in cm"
+      /><br/>
+      <label>Weight : </label>
+      <input
+        type="text"
+        onChange={(e) => setWeight(e.target.value)}
+        placeholder="Weight in kg"
+      /><br/>
+      <button onClick={handleBmi}>Calculate</button>
+    </div>  
+      <h2>BMI : {bmi}</h2>
+      <h2>STATUS : {info}</h2>
+    </div>
+  );
+};
+
+export default App;
